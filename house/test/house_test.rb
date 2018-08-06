a17c150b2ab6fbddd9120af4e79ea37bb08d4ba3
Sandi Metz <sandi@sandimetz.com>
2018-08-06 15:35:47 -0400

    Add 'from_one_dimensinal_array' to the series test name

--- house/test/house_test.rb
+++ house/test/house_test.rb
@@ -38,7 +38,7 @@ end
 
 
 class OrderedPhrasesTest < Minitest::Test
-  def test_series
+  def test_series_from_one_dimensional_array
     data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
     expected = "2a 2b 3a 3b the house that Jack built"
     assert_equal expected, OrderedPhrases.new(data: data).series(3)
