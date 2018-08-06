6e8cf938f26b86d42ddd8857f05000ebc40df503
Sandi Metz <sandi@sandimetz.com>
2018-08-06 14:56:32 -0400

    Add tests for Unchanged, Random and RandomButLast Orderers

--- house/test/house_test.rb
+++ house/test/house_test.rb
@@ -1,6 +1,33 @@
 require_relative '../../test_helper'
 require_relative '../lib/house'
 
+class UnchangedOrdererTest < Minitest::Test
+  def test_lines
+   Random.srand(1)
+   data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+   assert_equal data, UnchangedOrderer.new.order(data)
+ end
+end
+
+class RandomOrdererTest < Minitest::Test
+  def test_lines
+   Random.srand(1)
+   data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+   expected = ["the house that Jack built", "3a 3b", "1a 1b", "2a 2b"]
+   assert_equal expected, RandomOrderer.new.order(data)
+ end
+end
+
+class RandomButLastOrdererTest < Minitest::Test
+  def test_lines
+   Random.srand(1)
+   data     = ['1a 1b', '2a 2b', '3a 3b', 'the house that Jack built']
+   expected = ["1a 1b", "3a 3b", "2a 2b", "the house that Jack built"]
+   assert_equal expected, RandomButLastOrderer.new.order(data)
+ end
+end
+
+
 class HouseTest < Minitest::Test
   attr_reader :tale
   def setup
